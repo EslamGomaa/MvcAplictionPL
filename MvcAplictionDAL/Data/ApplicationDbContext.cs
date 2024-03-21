cf2d94fa4c0212b45c1a1e70316c8b95a6a2e13c
Eslam <eslamgomaa431@gmail.com>
2024-03-21 13:39:29 +0200

    DataAccess Videoes03

---
+++ MvcAplictionDAL/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcAplictionDAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvcAplictionDAL.Data
+{
+    internal class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseSqlServer("Server = . ; Database=MvcApplicaton ; Trusted_Connection =True ");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+        public DbSet<Department> Department { get; set; }
+    }
+}
---
+++ MvcAplictionDAL/Models/Department.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvcAplictionDAL.Models
+{
+    internal class Department
+    {
+        public int Id { get; set; }
+        // null the option type in c#  5.0
+        [Required(ErrorMessage ="Code is Required ya Eslam !!!")]
+        public string  Code  { get; set; }
+
+        public string Name { get; set; }
+        public DateTime DateOfCreation { get; set; } 
+            
+    }
+}
